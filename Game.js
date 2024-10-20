bd93b459bb9ba1bae19ac97c61e398c76710a8d6
mrhid3 <gkornas12@gmail.com>
2024-10-20 16:55:01 +0200

    bugfixy, spadanie, save przed wirusami

--- Game.js
+++ Game.js
@@ -12,7 +12,12 @@ export class Game{
         this.makeGameArray();
         let id = 1;
         let piece
-        piece = new Piece(id, "red", "blue", this);
+        let color1 = Math.floor(Math.random() * 3);
+        let color2 = Math.floor(Math.random() * 3);
+        piece = new Piece(id,
+            color1 === 0? "brown" :  color1 === 1? "yellow" : "blue",
+            color2 === 0? "brown" :  color2 === 1? "yellow" : "blue",
+            this);
         piece.ltg()
     }
 
@@ -20,17 +25,13 @@ export class Game{
         for(let i = 0; i < this.#height; i++){
             this.playingField[i]=[];
             for(let j = 0; j < this.#width; j++){
-                this.playingField[i][j] = [null, null];
+                this.playingField[i][j] = [null, null, null];
             }
         }
         for(let i = 0; i < this.numberOfViruses; i++){
             let x = Math.floor(Math.random() * 8);
             let y = Math.floor(Math.random() * 8) + 7;
-                // while(this.playingField[y][x][0] !== null){
-                //     let y = Math.floor(Math.random() * 10);
-                //     let x = Math.floor(Math.random() * 8);
-                // }
-            this.playingField[y][x][0] = (i % 3 === 0? "red": i % 3 === 1? "yellow": "blue");
+            this.playingField[y][x][0] = (i % 3 === 0? "brown": i % 3 === 1? "yellow": "blue");
             this.playingField[y][x][1] = 0;
             this.cont.children[y].children[x].style.backgroundColor = this.playingField[y][x][0];
             this.cont.children[y].children[x].style.border = "2px solid black"
@@ -44,63 +45,91 @@ export class Game{
             for(let j = 0; j < this.#width; j++){
                 let el = document.createElement("div");
                 el.classList.add("piece");
-                box.append(el)
-
+                box.append(el);
             }
-            this.cont.append(box)
+            this.cont.append(box);
         }
     }
 
     starDestroyer(){
+        let hasDestroyed = false;
         let copy = [...this.playingField];
         let copy2 = [...this.playingField];
-        for(let i = 0; i < this.#height; i++){
+        for(let i = 1; i < this.#height; i++){
             let counter = 1;
             for(let j = 1; j < this.#width; j++){
-                if(copy[i][j][0] !== null && copy[i][j][0] === copy[i][j - 1][0]){
-                    counter++;
+                if(copy[i][j][0] !== null){
+                    if(copy[i][j][0] === copy[i][j - 1][0]) {
+                        counter++;
+                    } else {
+                        if(counter >= 4) {
+                            for (let k = 0; k < counter; k++) {
+                                this.playingField[i][j - k - 1] = [null, null, null]
+                            }
+                            hasDestroyed = true;
+                            counter = 1;
+                        }else {
+                            counter = 1;
+                        }
+                    }
                 } else if(counter >= 4){
                     for(let k = 0; k < counter; k++){
-                        this.playingField[i][j - k - 1] = [null, null]
+                        this.playingField[i][j - k - 1] = [null, null, null]
                     }
-                    counter = 0;
+                    hasDestroyed = true;
+                    counter = 1;
                 }
             }
             if(counter >= 4){
                 for(let k = 0; k < counter; k++){
-                    this.playingField[i][7 - k] = [null, null]
+                    this.playingField[i][7 - k] = [null, null, null]
                 }
+                hasDestroyed = true;
             }
-            counter = 0
         }
         for(let j = 0; j < this.#width; j++) {
             let counter2 = 1;
             for (let i = 0; i < this.#height; i++) {
-                if (copy2[i][j][0] !== null && copy2[i][j][0] === copy2[i - 1][j][0]) {
-                    counter2++;
+                if (copy2[i][j][0] !== null){
+                    if(copy2[i][j][0] === copy2[i - 1][j][0]){
+                        counter2++;
+                    } else {
+                        if (counter2 >= 4) {
+                            for (let k = 0; k < counter2; k++) {
+                                this.playingField[i - k - 1][j] = [null, null, null]
+                            }
+                            counter2 = 1;
+                            hasDestroyed = true;
+                        }else {
+                            counter2 = 1;
+                        }
+                    }
                 } else if (counter2 >= 4) {
                     for (let k = 0; k < counter2; k++) {
-                        this.playingField[i - k - 1][j] = [null, null]
+                        this.playingField[i - k - 1][j] = [null, null, null]
                     }
-                    counter2 = 0;
+                    counter2 = 1;
+                    hasDestroyed = true;
                 }
             } if (counter2 >= 4) {
                 for (let k = 0; k < counter2; k++) {
-                    this.playingField[15 - k][j] = [null, null]
+                    this.playingField[15 - k][j] = [null, null, null]
                 }
+                hasDestroyed = true
             }
-            counter2 = 0
         }
         for(let i = 0; i < this.#height; i++){
             for(let j = 0; j < this.#width; j++){
                 if(this.playingField[i][j][0] === null){
-                    this.cont.children[i].children[j].style.backgroundColor = "transparent"
+                    this.cont.children[i].children[j].style.backgroundColor = "transparent";
                 }
             }
         }
+        if(hasDestroyed) this.fajnieSiedze();
     }
 
     fajnieSiedze(){
+        let hasfallen = false;
         for(let i = this.#height - 2; i > 0; i--){
             for(let j = 0; j < this.#width; j++){
                 if(this.playingField[i][j][0] !== null){
@@ -109,12 +138,14 @@ export class Game{
                     && ((j !== 7 && !(this.playingField[i][j][1] === this.playingField[i][j + 1][1] && this.playingField[i + 1][j + 1][0] !== null)) || (j === 7))
                     && ((j !== 0 && !(this.playingField[i][j][1] === this.playingField[i][j - 1][1] && this.playingField[i + 1][j - 1][0] !== null)) || (j === 0))){
                         this.cont.children[i].children[j].style.backgroundColor = "transparent";
-                        this.cont.children[i + 1].children[j].style.backgroundColor = this.playingField[i][j][0]
+                        this.cont.children[i + 1].children[j].style.backgroundColor = this.playingField[i][j][0];
                         this.playingField[i + 1][j] = [...this.playingField[i][j]];
-                        this.playingField[i][j] = [null, null]
+                        this.playingField[i][j] = [null, null, null];
+                        hasfallen = true;
                     }
                 }
             }
         }
+        if(hasfallen) this.starDestroyer()
     }
 }
